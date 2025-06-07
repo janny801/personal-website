8184a597be5363b7c0a9fc7efc93bae597ce2af8
janny801 <jred8069@gmail.com>
2025-06-07 13:00:12 -0500

    about me

--- personal-website/src/AboutMe.js
+++ personal-website/src/AboutMe.js
@@ -34,22 +34,26 @@ function AboutMe() {
 
       {/* About Me text */}
       <div className="about-me-text">
-        <p>
-          I'm a passionate computer science student at the University of Houston with interests in technology, and fitness.
-        </p>
-        <p>
-          My experience spans roles like developing databases for NASA and creating interactive applications, from AI-driven chatbots to real-time web platforms.
-        </p>
-        <p>
-          I am also a member of the Society of Asian Scientists and Engineers (SASE) at the University of Houston where I'm focusing on enhancing my soft skills and career preparation.
-        </p>
-        <p>
-          As someone who spends a lot of time on the internet, I find inspiration in the projects and websites that I come across. This fuels my passion to create projects that blend technical ability and creativity.
-        </p>
-        <p>
-          Currently, I'm seeking internship opportunities in software development or database management and development, where I can apply my skills and continue learning. Recently, I've also been developing an interest in cybersecurity, expanding my understanding of technology’s critical role in secure data management. With a focus on continuous growth, I'm excited to make impactful contributions in technology-driven fields.
-        </p>
-      </div>
+  <p>
+    I'm a passionate computer science student at the University of Houston with interests in technology, and fitness.
+  </p>
+  <p>
+    My experience spans roles like developing databases for NASA and creating interactive applications, from AI-driven chatbots to real-time web platforms.
+  </p>
+  <p>
+    I am also a member of the Society of Asian Scientists and Engineers (SASE) at the University of Houston where I'm focusing on enhancing my soft skills and career preparation.
+  </p>
+  <p>
+    I served as the Web Development Intern for SASE during the Spring 2025 semester and am currently the Web Development Officer for the Fall 2025 – Spring 2026 school year.
+  </p>
+  <p>
+    As someone who spends a lot of time on the internet, I find inspiration in the projects and websites that I come across. This fuels my passion to create projects that blend technical ability and creativity.
+  </p>
+  <p>
+    Currently, I'm seeking internship opportunities in software development or database management and development, where I can apply my skills and continue learning. Recently, I've also been developing an interest in cybersecurity, expanding my understanding of technology’s critical role in secure data management. With a focus on continuous growth, I'm excited to make impactful contributions in technology-driven fields.
+  </p>
+</div>
+
 
       {/* Slideshow container */}
       <div className="slideshow">
